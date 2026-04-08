0de2436824a59610a3b12b8698dbcaa19abb1ef4
priyanka <priyankaaparmar0707@gmail.com>
2026-04-08 23:22:48 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from typing import List
+
+def get_requirements(file_path:str) -> List[str]:
+    
+    reqirement_list = list[str] = []
+    
+    return reqirement_list
+
+
+
+
+
+
+setup(
+    name = 'sensor',
+    
+    version = '0.0.1',
+    author = 'priyanka',
+    author_email = 'priyankaparmar0707@gmail.com',
+    packages = find_packages(), 
+    install_requires= get_requirements()
+)
